42a43f672bd42bca201a7ab59bb9115c9448ffd5
Rey <reydavidquiroz@gmail.com>
2024-09-18 20:08:30 -0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,9 @@
-import React from "react";
+import React from 'react'
 
 function Footer() {
   return (
-    <header className="bg-gradient-to-r from-pink-500 to-red-500 p-6 text-center mt-10">
-      <h1 className="md:text-4xl sm:text-3xl font-bold text-white drop-shadow-lg mb-5">
-        ¡Regalito Adelantado Con Mucho Amor <br/> Mi Branquita Bella!
-      </h1>
-    </header>
-  );
+    <div>Footer</div>
+  )
 }
 
-export default Footer;
+export default Footer
